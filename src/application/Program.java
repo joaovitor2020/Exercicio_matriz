964cd5543be41a7b39ca5dffdf688f69152cc168
Nickolas Carlos <nickolascarlos@users.noreply.github.com>
2021-11-04 02:14:30 -0300

    Tradução

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,18 +19,18 @@ public class Program {
 		for(int i = 0 ; i<mat.length; i++) {
 			for(int j = 0; j< mat[i].length; j++) {
 				if (mat[i][j] == x) {
-					System.out.println("Position "+ i + "," + + j + ":");
+					System.out.println("Posição "+ i + "," + + j + ":");
 					if(j - 1 >= 0) {
-						System.out.println("Left: " + mat[i][j-1]);
+						System.out.println("Esquerda: " + mat[i][j-1]);
 					}
 					if(j+1 < n) {
-						System.out.println("Right: " + mat[i][j+1]);
+						System.out.println("Direita: " + mat[i][j+1]);
 					}
 					if(i - 1 >= 0) {
-						System.out.println("Up: " + mat[i-1][j]);
+						System.out.println("Cima: " + mat[i-1][j]);
 					}
 					if(i + 1 < m) {
-						System.out.println("Down: " + mat[i+1][j]);
+						System.out.println("Baixo: " + mat[i+1][j]);
 					}
 					
 				}
